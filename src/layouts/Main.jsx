12a96aa7e9f079e236468e7f9d7d159d6818a759
developersaddam <developerssaddam@gmail.com>
2024-05-16 18:37:08 +0600

    Added Footer page

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Main = () => {
   return (
     <div>
-        <Outlet/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
